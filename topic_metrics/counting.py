dfc8ce610f041736f66ac3c22194883e9710a9a2
jiapeng <jiapeng1995@gmail.com>
2025-08-02 05:46:52 +0000

    improve performance of counting co-occurrences

--- topic_metrics/counting.py
+++ topic_metrics/counting.py
@@ -4,10 +4,11 @@ import math
 import numpy as np
 import pickle
 
+
 from collections import defaultdict
 from functools import reduce, partial
-from itertools import combinations
-from multiprocessing import Pool, Array
+from multiprocessing import Pool, Array, Manager
+from numpy.lib.stride_tricks import sliding_window_view
 from time import time
 from tqdm import tqdm
 
@@ -169,63 +170,23 @@ def count_windows_helper(f, directory, window_size, vocab2id):
 
     Return
     ------
-    single_count : Dict[int, int]
-    joint_count : Dict[int, Dict[int, int]]
+    None (edits shared array)
     """
 
-    single_count = defaultdict(int)
-    joint_count = defaultdict(lambda: defaultdict(int))
-    MASK = np.ones(window_size, dtype=bool)
-
     with open(f'{directory}/{f}', 'r') as f:
-        docs = [line.rstrip() for line in f]
+        for line in f:
+            tokens = [vocab2id[w] for w in line.rstrip().split() if w in vocab2id]
+            if len(tokens) <= window_size or window_size == 0:
+                with glock:
+                    shared_joint_array[np.ix_(tokens,tokens)] += 1
+            else:
+                ixs = [np.ix_(arr,arr) for arr in sliding_window_view(tokens, window_shape=window_size)]
+                with glock:
+                    for ix in ixs:
+                        shared_joint_array[ix] += 1
+            
 
-    for i in range(len(docs)):
-        tokens = docs[i].split()
-        length = len(tokens)
-        doc = defaultdict(list)
-        for i, t in enumerate(tokens):
-            if t in vocab2id:
-                doc[t].append(i)
-        words = list(doc.keys())
-
-        if window_size >= length or window_size == 0:
-            for w in words:
-                single_count[vocab2id[w]] += 1
-            for w1, w2 in combinations(words, 2):
-                joint_count[vocab2id[w1]][vocab2id[w2]] += 1
-        else:
-            convolved_bms = {}
-            for w in words:
-                bm = np.zeros(length, dtype=bool)
-                bm[np.array(doc[w])] = True
-                convolved_bms[w] = np.convolve(bm, MASK, mode='valid')
-                single_count[vocab2id[w]] += convolved_bms[w].sum()
-
-            for w1, w2 in combinations(words, 2):
-                joint_count[vocab2id[w1]][vocab2id[w2]] += np.logical_and(
-                    convolved_bms[w1], convolved_bms[w2]
-                ).sum()
-
-    single_count = {k1: v for k1, v in single_count.items() if v != 0}
-    joint_count = {k1: {k2: v for k2, v in s.items() if v != 0}
-                   for k1, s in joint_count.items()}
-
-    words = list(single_count.keys())
-    for w in words:
-        if w not in joint_count:
-            joint_count[w] = {}
-    for w1, w2 in combinations(words, 2):
-        rhs = joint_count[w2][w1] if w1 in joint_count[w2] else 0
-        lhs = joint_count[w1][w2] if w2 in joint_count[w1] else 0
-        if rhs + lhs == 0:
-            continue
-        joint_count[w2][w1] = joint_count[w1][w2] = rhs + lhs
-
-    return single_count, joint_count
-
-
-def init_bases(single_base, joint_base, size):
+def init_base(joint_base, size, lock):
     """Initializer for shared array between pool workers
     Parameters
     ----------
@@ -234,30 +195,11 @@ def init_bases(single_base, joint_base, size):
     size : int
         size x size matrix
     """
-    global shared_single_array
+    global glock
     global shared_joint_array
-
-    shared_single_array = np.ctypeslib.as_array(single_base.get_obj())
+    glock = lock
     shared_joint_array = np.ctypeslib.as_array(joint_base.get_obj())
     shared_joint_array = shared_joint_array.reshape(size, size)
-
-
-def count_windows_multiprocessing(f, directory, window_size, vocab2id):
-    """ helper for count_windows, require init
-    Parameters
-    ----------
-    refer to count_windows_helper
-    """
-    single, joint = count_windows_helper(
-        f, directory, window_size, vocab2id)
-    for k1, v in single.items():
-        shared_single_array[k1] += v
-
-    for k1, s in joint.items():
-        inc = np.zeros(len(shared_joint_array), dtype=np.int64)
-        for k2, v in s.items():
-            inc[k2] += v
-        shared_joint_array[k1, :] += inc
 
 
 def count_windows(directory, destination, window_size, vocab2id,
@@ -292,24 +234,24 @@ def count_windows(directory, destination, window_size, vocab2id,
     os.makedirs(dest_joint, exist_ok=True)
 
     files = [f for f in os.listdir(directory) if f.endswith('.txt')]
-    shared_single_base = Array(ctypes.c_long, len(vocab2id))
     shared_joint_base = Array(ctypes.c_long, len(vocab2id)**2)
 
-    with Pool(processes=count_processes, initializer=init_bases,
-              initargs=(shared_single_base, shared_joint_base, len(vocab2id))
+    mgr = Manager()
+    lock = mgr.Lock()
+    with Pool(processes=count_processes, initializer=init_base,
+              initargs=(shared_joint_base, len(vocab2id), lock)
               ) as pool:
-        pool.map(partial(count_windows_multiprocessing,
+        pool.map(partial(count_windows_helper,
                          directory=directory,
                          window_size=window_size, vocab2id=vocab2id),
                  tqdm(files))
 
     print(f'counting completed, {time()-inittime} seconds, dumping...')
-    shared_single_array = np.ctypeslib.as_array(shared_single_base.get_obj())
+    
     shared_joint_array = np.ctypeslib.as_array(shared_joint_base.get_obj())
     shared_joint_array = shared_joint_array.reshape(
         len(vocab2id), len(vocab2id))
-
-    pickle.dump({i: v for i, v in enumerate(shared_single_array)},
+    pickle.dump({i: v for i, v in enumerate(np.diag(shared_joint_array))},
                 open(f"{destination}/single.pkl", "wb"))
     for k1 in range(len(vocab2id)):
         pickle.dump({k2: v for k2, v in enumerate(shared_joint_array[k1, :])
